57d82c1f5ccc1de5b83255ae996dd9349741f2c3
Manas Karadia <manaskaradia@gmail.com>
2023-10-19 12:49:59 +0530

    sample

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('SaveGuart.iot'),
+        title: Text('SaveGuart.iot '),
       ),
       body: Center(
         child: Column(
