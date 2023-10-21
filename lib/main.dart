78a84dc638246dc3844a31c9199210d906513fe9
Mukund Solanki <mukundsolanki665@gmail.com>
2023-10-21 15:05:20 +0530

    Sucessfully fetching the IR sensor data

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyHomePageState extends State<MyHomePage> {
   String status = 'Loading...';
 
   void getStatus() async {
-    final Uri url = Uri.parse('http://192.168.29.80:5000');
+    final Uri url = Uri.parse('http://192.168.29.46:80');
     try {
       final response = await http.get(url);
       if (response.statusCode == 200) {
@@ -55,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Obstacle Status'),
+        title: Center(child: Text('SAVE GUARD IOT')),
       ),
       body: Center(
         child: Column(
